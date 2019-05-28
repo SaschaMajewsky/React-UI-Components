cf2435c4b8af38b2568766c3b21ed707dd75e7af
Sascha <sascha.majewsky93@gmx.de>
2019-05-28 20:48:59 +0200

    finished the stretchgoal on the social card project with javascript and momentjs integration

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Footer.css";
 
 function Footer() {
+    const [isRed, setRed] = useState(false);
+    const [count, setCount] = useState(0);
+
+    const [isRetweet, setRetweet] = useState(false);
+    const [countRetweet, setRetweetCount] = useState(0);
+
+    const incrementHeart = () => {
+        setRed(!isRed);
+        isRed ? setCount(count - 1) : setCount(count + 1);
+    }
+
+    const incrementRetweet = () => {
+        setRetweet(!isRetweet);
+        isRetweet ? setRetweetCount(countRetweet - 1) : setRetweetCount(countRetweet + 1);
+    }
     return (
-        <footer>
+        <footer className="footerContent">
+            <img className="speak symbol" src="https://i.imgur.com/0mNf9cZ.png" alt='A grey symbol of a speak quote on a grey background.'/>
+            <img onClick={incrementRetweet} className="retweet symbol" src="https://i.imgur.com/ZCpNj9a.png" alt='A grey symbol of two arrows bennt into a circle on a grey background.'/>
+            <span className="retweetCounter">{countRetweet}</span>
+            <img onClick={incrementHeart} className={isRed ? "heart filled symbol" : "heart symbol"} src="https://i.imgur.com/WPpIRFI.png" alt='A grey symbol of a heart on a grey background.'/>
+            <span className="heartCounter">{count}</span>
+            <img className="mail symbol" src="https://i.imgur.com/dCvbcVz.png" alt='A grey symbol of a post mail on a grey background.'/>
         </footer>
     );
 }
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Header.css';
 import HeaderContent from "./HeaderContent";
+import moment from 'moment'
 
 function HeaderTitle() {
+    let today = moment().format("ll");
     return (
         <div className="header">
             <div className="header-title">
                 <h2>Lambda School</h2>
                 <a href={'https://www.lambdaschool.com/'}>@LambdaSchool</a>
-                <p> • 26 Jan</p>
+                <p> {today.split(' ')[1].replace(',','') + " " + today.split(' ')[0]}</p>
             </div>
             <div className="header-content">
                 <HeaderContent />
